9e68fcc43cc192febe6e4521ae0e06e6f0d8ca43
mjs111 <101532550+mjs111@users.noreply.github.com>
2022-03-15 20:50:40 -0700

    first publish

---
+++ UpdateStockPrices.command
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 /Users/michaelsandrik/Documents/Finance/Python/UpdateStockPrices.py
---
+++ UpdateStockPrices.py
@@ -0,0 +1,34 @@
+import openpyxl
+import os
+import yfinance
+
+# dir = "/Users/michaelsandrik/Documents/Finance/Stock Valuations/"
+dir = "/users/michaelsandrik/Library/Mobile Documents/com~apple~CloudDocs/Documents/Finance/Stock Valuations/"
+
+dir_list = os.listdir(dir)
+
+for file in dir_list:
+    if file.endswith(".xlsx"):  # only act on Excel files
+
+        tilde = "~"  # check for Excel temp files and skip them
+        if tilde in file:
+            continue
+
+        print("Opening", file)
+
+        wb = openpyxl.load_workbook(dir + file)
+        # ws = wb.active                    #price is on is always the first sheet, so can go with active
+        ws = wb["Valuation"]  # set active worksheet explicitly
+
+        stock_ticker = ws["C4"].value  # get stock ticker from spreadsheet
+        print("Updating", stock_ticker)
+        print("old price", ws["B16"].value)
+
+        yf_stock = yfinance.Ticker(stock_ticker)  # get stock data from Yahoo Finance
+        price = yf_stock.info["regularMarketPrice"]
+
+        ws["B16"].value = price  # update stock price value in spreadsheet
+
+        print("new price", ws["B16"].value, "\n")
+
+        wb.save(dir + stock_ticker + ".xlsx")
